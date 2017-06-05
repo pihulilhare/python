e5bd215819178c2ae25a24b99140fd0f76e04bea
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 17:49:44 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,13 @@
+#2. Write a python script which will display system name ,node name and release details of current system.
+
+#display node
+
+import os
+os.uname().nodename
+import platform
+platform.node()
+platform.system()
+platform.release()
+platform.version()
+import socket
+socket.gethostname()
