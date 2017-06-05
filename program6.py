7e991f194131534c9c042917b4c87dcf0eea6ad8
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-06 00:09:01 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -1,6 +1,6 @@
 #6. Write a program that takes inputs from user their name and their age. And display the year in which they will turn 100 years old.
 
-name = input("What is your name: ")
-age = int(input("How old are you: "))
+name = raw_input("What is your name: ")
+age = int(raw_input("How old are you: "))
 year = str((2014 - age)+100)
 print(name + " will be 100 years old in the year " + year)
