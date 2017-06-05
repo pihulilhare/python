56f334b70a11923509a5adfa48e775c5a9c0f3c3
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 18:16:39 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,3 +1,4 @@
+
 #1. Write a python script for creating directory,displaying its contents. 
 
 # Create a file named Directory1.py
@@ -12,3 +13,6 @@ os.chdir("Priyanka")
 os.system("touch file1")
 os.system ("ls") # it will list all the files and directories
 os.system("cat file1") 
+'''OUTPUT
+program1.py
+file1'''
