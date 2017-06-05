ce910faf5b0fc65742d288d834de2679ce00c3fa
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 22:06:57 +0530

    Update program14.py

--- program14.py
+++ program14.py
@@ -1,7 +1,9 @@
 #14. Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
-
 s = raw_input("Enter the ASCII Value")
 u = unicode( s ,"utf-8")
 print u
+'''output
+Enter the ASCII Value 97
+ 97'''
 
 
