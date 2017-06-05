55d50f6b889fa8ea989671e9644fa7d32a66d8a9
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 23:44:05 +0530

    Update program9.py

--- program9.py
+++ program9.py
@@ -1,11 +1,8 @@
 #9. Define a function max_of_three() that takes three numbers as arguments and returns the largest of them.  
 
-function maxOfThree(a, b, c):
-	if ((a >= b) && (a >= c)) :  
-		return a;
-	else if ((b >= a) && (b >= c)):
-		return b;
-	else:	
-		return c;
-    
-print(maxOfThree(343,35124,42))
+import max
+def max_of_three(a, b, c):
+    max_temp = max(a, b)
+    return max(max_temp, c)
+if __name__ == "__main__":
+    print max_of_three(1, 2, 3)
