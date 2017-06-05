f1f6c5f54b159f95d0876cb87adc6d036f839bcb
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-06 00:10:16 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -4,3 +4,8 @@ name = raw_input("What is your name: ")
 age = int(raw_input("How old are you: "))
 year = str((2014 - age)+100)
 print(name + " will be 100 years old in the year " + year)
+
+'''output
+What is your name: Priyanka Lilhare
+How old are you: 23
+Priyanka Lilhare will be 100 years old in the year 2091'''
