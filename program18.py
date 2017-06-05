fb0db299d08590c839921d31539f1f60640367f9
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 21:19:59 +0530

    Update program18.py

--- program18.py
+++ program18.py
@@ -1,6 +1,9 @@
 # 8. Write a program which accepts a string from console and print it in reverse order.
-
-
 s=raw_input("Enter String")
 s = s[::-1]
 print s
+
+#output
+'''[root@test python]# python program18.py
+Enter String Hi this program is for reverse string
+gnirts esrever rof si margorp siht iH '''
