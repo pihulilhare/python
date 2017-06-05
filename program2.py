1b4ef620fc59ecdd63217511afbe3cf59c60bdc8
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 19:20:20 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,13 +1,9 @@
 #2. Write a python script which will display system name ,node name and release details of current system.
-
 #display node
-
-import os
+import os,platform,socket
 os.uname().nodename
-import platform
 platform.node()
 platform.system()
 platform.release()
 platform.version()
-import socket
 socket.gethostname()
