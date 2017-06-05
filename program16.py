bec846cd5c9c04f32b1cf5dbbb0f23c425b0176b
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 22:02:47 +0530

    Update program16.py

--- program16.py
+++ program16.py
@@ -1,6 +1,4 @@
 #16. Write a program to print the list after removing delete even numbers in [5,6,77,45,22,12,24].
-
-
 def remove_even(l):
     for i in l:
         if i % 2 != 0:
@@ -11,3 +9,7 @@ def remove_even(l):
 remove_even([5,6,77,45,22,12,24])
 remove_even([4,5,4,7,9,11])
 remove_even([5,6,77,45,22,12,24])
+'''output
+[6, 45, 22, 12, 24]
+[4, 4, 9]
+[6, 45, 22, 12, 24]'''
