740be1eea63e6a6245a782c04cdeab9398619b8f
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-05 23:42:26 +0530

    Update program10.py

--- program10.py
+++ program10.py
@@ -2,15 +2,12 @@
 
 print ("")
 print ("Function to find the length of the List")
- 
 List = [2,1,8,6,5,9,3]
- 
 print ("The list is " +str(List))
 def countListLength(List):
 	listCount = 0
- 
 for i in List:
 	listCount = listCount+1
 	return listCount
- 
+
 print("Length of the list is " +str(countListLength(List)))
