56f8d695d523c301b45cfe60958bed253701477f
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-06 11:27:31 +0530

    Update program7.py

--- program7.py
+++ program7.py
@@ -1,6 +1,6 @@
 #7. Python script which accept name from command line and print it to standard output.
-x = raw_input('What is your name?')
-print 'Your name is ' + x
+A = raw_input('What is your name?')
+print 'Your name is ' + A
 
 '''output
 [root@test python]# python program7.py
